2b75374d8b6e3f24763c009e61d2a6e6b0a1b33c
Andrew Pereira <andrew.jpb@outlook.com>
2022-11-23 13:14:23 -0300

    cli: fix test error

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,13 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-
-
-
 describe('AppController', () => {
   let appController: AppController;
   let appService: AppService;
-  
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -21,9 +17,8 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return { ok:true }', () => {
-      const result = {ok:true}
+      const result = { ok: false };
       expect(appController.getHello()).toEqual(result);
-
     });
   });
 });
